c43f05b9bad4019d9d2f945c48da4ccaa558a367
Katherine Golden <36462958+TimeLordKEG@users.noreply.github.com>
2019-02-02 21:55:02 -0600

    Add files via upload

---
+++ SandwichShop.java
@@ -0,0 +1,86 @@
+package com.jetbrains;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+
+    public static void main(String[] args)
+    {
+	    Scanner keyboard = new Scanner(System.in);
+	    int goalForVeggies = 50;
+	    int goalForBurgers =250;
+	    int goalForSubs =180;
+	    int goalForSoup = 70;
+
+        int veggieSadwichesSold;
+	    System.out.println("Checking sales goals.");
+        System.out.println("The sales goal for veggies sandwiches is " + goalForVeggies);
+		System.out.println("How many veggie sandwiches were sold today?");
+		veggieSadwichesSold = keyboard.nextInt();
+
+		if (veggieSadwichesSold >= goalForVeggies)
+        {
+            System.out.println("\nMade goal for veggies.");
+        }
+
+		else
+        {
+
+			System.out.println("\nFell short.");
+		}
+
+        int burgersSold;
+        System.out.println("The sales goal for burgers is " + goalForBurgers);
+        System.out.println("How many veggie sandwiches were sold today?");
+        burgersSold = keyboard.nextInt();
+
+        if (burgersSold >= goalForBurgers)
+        {
+            System.out.println("\nMade goal for burgers.");
+        }
+
+        else
+        {
+
+            System.out.println("\nFell short.");
+        }
+
+        int subsSold;
+        System.out.println("The sales goal for veggies subs is " + goalForSubs);
+        System.out.println("How many veggie sandwiches were sold today?");
+        subsSold = keyboard.nextInt();
+
+        if (subsSold >= goalForSubs)
+        {
+            System.out.println("\nMade goal for subs.");
+        }
+
+        else
+        {
+
+            System.out.println("\nFell short.");
+        }
+
+        int soupSold;
+        System.out.println("The sales goal for veggies sandwiches is " + goalForSoup);
+        System.out.println("How many veggie sandwiches were sold today?");
+        soupSold = keyboard.nextInt();
+
+        if (soupSold >= goalForSoup)
+        {
+            System.out.println("\nMade goal for soup.");
+        }
+
+        else
+        {
+
+            System.out.println("\nFell short.");
+        }
+
+        if (veggieSadwichesSold >= goalForVeggies && burgersSold >= goalForBurgers && subsSold >= goalForSubs && soupSold >= goalForSoup)
+        {
+            System.out.println("\nMade goal for everything!");
+        }
+    }
+
+}
